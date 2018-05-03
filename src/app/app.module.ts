0659272fd1a03566e4031fb8e47d03a419dabbef
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-05-03 12:39:47 +0530

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NewsfeedComponent } from './components/newsfeed/newsfeed.component';
 import { ChatComponent } from './components/chat/chat.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { APP_ROUTES } from './app.routing';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NewsfeedComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NewsfeedComponent } from './components/newsfeed/newsfeed.component';
+import { ChatComponent } from './components/chat/chat.component';
+
+export const APP_ROUTES: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/login'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'home',
+                component: NewsfeedComponent
+            },
+            {
+                path: 'chat',
+                component: ChatComponent
+            }
+        ]
+    }
+];
