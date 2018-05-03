96c23bdd0b0561a620dadffa3eba154b49dd712f
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2018-05-03 11:10:08 +0530

    basic introduction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NewsfeedComponent } from './components/newsfeed/newsfeed.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsfeedComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
